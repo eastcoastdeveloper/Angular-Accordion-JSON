518825193630139615bfa87113c018dee80f41a5
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2022-11-03 08:16:40 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AccordionData } from './accordion.interface';
-// import * as data from './data.json';
 
 @Component({
   selector: 'my-app',
@@ -13,29 +12,34 @@ export class AppComponent implements OnInit {
 
   @ViewChild('accordionParent', { static: false }) accordionParent: ElementRef;
 
-  constructor(private _http: HttpClient) {
-    
-  }
+  constructor(private _http: HttpClient) {}
 
   ngOnInit(): void {
-    this._http.get('assets/data.json').subscribe(val => {
-      console.log(val)
-    })
-  //   for (let key in data.items) {
-  //     if (data.items.hasOwnProperty(key)) {
-  //       this.accordionData.push(data.items[key]);
-  //     }
-  //   }
+    this._http.get<AccordionData[]>('assets/data.json').subscribe((val) => {
+      this.accordionData = val;
+    });
   }
 
   // Toggle Accordion
   toggleSection(e: any, index: number) {
     var allGroups = [];
-    allGroups = this.accordionParent.nativeElement.getElementsByClassName('hidden-content');
+    allGroups =
+      this.accordionParent.nativeElement.getElementsByClassName(
+        'hidden-content'
+      );
     for (var j = 0; j < allGroups.length; j++) {
-      if (index === j && !e.target.parentElement.querySelector('.hidden-content').classList.contains('show-content')) {
-        e.target.parentElement.querySelector('.hidden-content').classList.add('show-content');
-      } else { allGroups[j].classList.remove('show-content'); }
+      if (
+        index === j &&
+        !e.target.parentElement
+          .querySelector('.hidden-content')
+          .classList.contains('show-content')
+      ) {
+        e.target.parentElement
+          .querySelector('.hidden-content')
+          .classList.add('show-content');
+      } else {
+        allGroups[j].classList.remove('show-content');
+      }
     }
   }
 }
